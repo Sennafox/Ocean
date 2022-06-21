a448eae54e65a0733c9ce6891bb66268dadad75d
PAULO BITTENCOURT/LGESP HE/BS SQA Local Part - MAO(paulo.bittencourt@lge.com) <paulo.bittencourt@lge.com>
2022-06-20 21:40:37 -0400

    CRUD completo

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express  = require('express')
+const bodyParser  = require('body-parser')
 const app = express()
 
 const URL = 3000;
+
+app.use(bodyParser.json())
 
 app.get('/hello', (req, res) => {
     res.send('Hello World')
@@ -12,9 +15,9 @@ app.get('/hello', (req, res) => {
     CRUD: Create, Read (Single & All), Update and Delete
     - [GET] /mensagens - Retorna lista de mensagens
     - [GET] /mensagens/id - Retorna apenas uma única mensagem pelo id
-    - [GET] /mensagens - Cria uma nova mensagem
-    - [GET] /mensagens - Atualiza uma mensagem pelo id
-    - [GET] /mensagens - Remove uma mensagem pelo id
+    - [POST] /mensagens - Cria uma nova mensagem
+    - [PUT] /mensagens - Atualiza uma mensagem pelo id
+    - [DELETE] /mensagens - Remove uma mensagem pelo id
 */
 
 const mensagens = [
@@ -25,7 +28,7 @@ const mensagens = [
 //- [GET] /mensagens - Retorna lista de mensagens
 
 app.get('/mensagens', (req, res) =>{
-    res.send(mensagens)
+    res.send(mensagens.filter(Boolean))
 })
 
 //- [GET] /mensagens/id - Retorna apenas uma única mensagem pelo id
@@ -33,6 +36,36 @@ app.get('/mensagens/:id', (req, res) =>{
     const id = req.params.id-1;
     const mensagem = mensagens[id];
     res.send(mensagem);
+})
+
+//- [POST] /mensagens - Cria uma nova mensagem
+app.post('/mensagens', (req, res) =>{
+    const mensagem = req.body.mensagem;
+
+    mensagens.push(mensagem);
+
+    res.send(`Nova mensagem criada: '${mensagem}'`);
+})
+
+app.put('/mensagens/:id', (req, res) =>{
+
+    const id = req.params.id - 1;
+
+    const mensagem = req.body.mensagem;
+    
+    mensagens[id] = mensagem;
+
+    res.send(`Mensagem ${id+1} atualizada com sucesso: '${mensagem}'`)
+
+})
+app.delete('/mensagens/:id', (req, res) =>{
+
+    const id = req.params.id - 1;
+    
+    delete mensagens[id];
+
+    res.send(`Mensagem ${id+1} removida com sucesso!`)
+
 })
 
 app.listen(URL , () => {
